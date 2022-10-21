c8b73478c90fe1d0de1942c8bbfca08ca3c7dd31
rajikrishnan2002 <116347396+rajikrishnan2002@users.noreply.github.com>
2022-10-21 14:51:23 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+print( "hello world")
+a=int(input("enter a value"))
+print(a)
+print("your number is  ",a)
+b=int(input("enter another value"))
+print(b)
+print("your second number is",b)
+c=a+b
+print("the sum is",c)
+d=a*b
+print("the product is",d)
+e=a/b
+print("the division is",e)
+
+
